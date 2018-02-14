231096269b3614043e571f5ffe0dfbae1432c0b1
vincentsun86 <32657620+vincentsun86@users.noreply.github.com>
2018-02-14 13:46:06 +0900

    Create Appropriate Text Field for the Input

---
+++ src/LoginTextField.swift
@@ -0,0 +1,30 @@
+//
+//  LoginTextField.swift
+//  JPCC 1.0
+//
+//  Created by Vincent Sun on 2017/12/21.
+//  Copyright © 2017年 Vincent Sun. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LoginTextField: UITextField {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.borderColor = UIColor(white: 231 / 255, alpha:1).cgColor
+        self.layer.borderWidth = 1
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 8, dy: 7)
+    }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return textRect(forBounds: bounds)
+    }
+
+    
+}
